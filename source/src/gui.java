ffff121ba5444f05acf900e6c7bf0ed5d9c068b9
molo2 <molotheepiceyego@gmail.com>
2025-01-02 03:12:59 -0800

    - made main file - made a blank window thats it.

---
+++ source/src/gui.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class gui {
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("blackjack 2.0");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null); //sets frame to middle of the screen
+
+
+    }
+}
